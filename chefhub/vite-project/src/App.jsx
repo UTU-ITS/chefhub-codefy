c8eae27bdd3566435a1599d11d800f6be688e192
vjesus2004 <jesus-_044@hotmail.com>
2024-10-07 17:58:34 -0300

    CheffhubV2

--- chefhub/vite-project/src/App.jsx
+++ chefhub/vite-project/src/App.jsx
@@ -3,19 +3,24 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import LoginView from './components/LoginView'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+   <NavBar></NavBar>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LoginView/>}/>
      <Route path="/home" element={<Home/>}/>
+     <Route path="/menu" element={<Menu/>}/>
+
+     
     </Routes> 
      
-      </BrowserRouter>
+    </BrowserRouter>
     </>
   )
 }
