23024fea439e3d78a5eddd2a9cd0b1904f1b2bb2
MarceDS07 <marce.de.souza.25@gmail.com>
2024-10-09 17:12:43 -0300

    Prueba Commit

--- chefhub/vite-project/src/components/Home.jsx
+++ chefhub/vite-project/src/components/Home.jsx
@@ -6,7 +6,13 @@ import './Home.css'
 export default function Home() {
   return (
     <div className='home-div'>
-      <h1>Home</h1>
+      <div className="home-div-box">
+        <div className="home-div-content">
+          <h1 className="home-h1">Bienvenido a Chef Hub</h1>
+          <p className="home-p">La mejor plataforma para encontrar recetas de cocina</p>
+          <a href="/menu" className="btn">Ver Menu</a>
+        </div>
+      </div>
     </div>
   )
 }
--- chefhub/vite-project/src/components/Menu.jsx
+++ chefhub/vite-project/src/components/Menu.jsx
@@ -15,6 +15,8 @@ export default function Menu() {
     </ChakraProvider>
     </div>
 
+
+
     </>
   )
 }
