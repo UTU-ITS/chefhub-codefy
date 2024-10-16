6fffcb0a1be2ccf1ef32095637deaa9c3c599ac4
vjesus2004 <jesus-_044@hotmail.com>
2024-10-16 20:37:22 -0300

    Api changes
    multiples endpoints por index.php

--- chefhub/vite-project/src/components/Menu.jsx
+++ chefhub/vite-project/src/components/Menu.jsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import Categories from './Categories';
 import Product from './Product';
 import './Menu.css';
+import Cart from './Cart';
 
 export default function Menu() {
   // Estado para la categoría seleccionada
@@ -17,21 +18,20 @@ export default function Menu() {
     <>
       <div className='menu-div'>
         <div className='filters'>
+          <Cart/>
           <h1 className='filters-title'>Filtros</h1>
-          {/* Clase condicional: cambia el color si se selecciona "Todos" */}
+         
           <button 
             className={`btn-tag ${selectedCategory === null ? 'selected' : ''}`} 
             onClick={handleSelectAll}
           >
             Todos
           </button>
-          {/* Pasar setSelectedCategory como callback para actualizar la categoría */}
           <Categories selectedKey={selectedCategory} onSelectKey={setSelectedCategory} />
         </div>
 
         <div className="catalog">
           <ChakraProvider>
-            {/* Pasar la categoría seleccionada como prop */}
             <Product selectedKey={selectedCategory} onSelectKey={setSelectedCategory} />
           </ChakraProvider>
         </div>
