671edbff03e0d3e2cd356b71d557dadac67fcc2a
vjesus2004 <jesus-_044@hotmail.com>
2024-10-23 17:21:58 -0300

    v123

--- chefhub/vite-project/src/components/Home/NavBar.jsx
+++ chefhub/vite-project/src/components/Home/NavBar.jsx
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './NavBar.css';
 import { UserIcon } from '../../img/HeroIcons';
+import Logo from '../../assets/logo.svg';
 
 const NavBar = () => {
   return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container">
           <a className="navbar-brand" href="/">
-            <img  alt="Chef Hub Logo" style={{ width: '60px' }} />
+            <img  alt="Chef Hub Logo" src={Logo} style={{ width: '60px' }} />
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -20,14 +21,11 @@ const NavBar = () => {
                 <a className="nav-link" href="/menu">Menu</a>
               </li>
               <li className="nav-item"> 
-     
+                <a className="nav-link" href="/login">
                   <button className='nav-item'>
-                    Iniciar Sesion
-                  <UserIcon/>
+                    <UserIcon/>
                   </button>
-
-    
-                <a className="nav-link" href="/login"></a>
+                </a>
               </li>
             </ul>
           </div>
