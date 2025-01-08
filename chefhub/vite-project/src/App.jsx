7f8b19cf32a722c45d794c9c2a9c916f8d235e0c
Marcos <dacostamarcos25@gmail.com>
2025-01-07 21:57:56 -0300

    Merge completo y solucion de errores (resta corregir register)

--- chefhub/vite-project/src/App.jsx
+++ chefhub/vite-project/src/App.jsx
@@ -1,58 +1,34 @@
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
-import './App.css'
-import NavBar from './components/Home/NavBar'
-import Home from './components/Home/Home'
-import LoginView from './components/Login-Register/LoginView'
-import Menu from './components/Shop/Menu'
-import Checkout from './components/Shop/Checkout'
-import AdminOptions from './components/Admin/AdminOptions'
-import RegisterView from './components/Login-Register/RegisterView'
-import { CartProvider } from './context/cart'
-import AddProduct from './components/Admin/Products/AddProduct'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/Home/NavBar';
+import Home from './components/Home/Home';
+import LoginView from './components/Login-Register/LoginView';
+import Menu from './components/Shop/Menu';
+import Checkout from './components/Shop/Checkout';
+import AdminOptions from './components/Admin/AdminOptions';
+import RegisterView from './components/Login-Register/RegisterView';
+import { CartProvider } from './context/cart';
+import AddProduct from './components/Admin/Products/AddProduct';
 
 function App() {
+
   return (
     <CartProvider>
       <BrowserRouter>
-        <MainApp />
+      <NavBar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginView />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/register" element={<RegisterView />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/admin/*" element={<AdminOptions />} />
+            <Route path="/admin/products/addproduct" element={<AddProduct />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </CartProvider>
-  );
-}
-
-function MainApp() {
-  const location = useLocation(); // Hook para obtener la ruta actual
-
-  // Oculta NavBar para cualquier ruta que comience con "/admin"
-  const shouldShowNavBar = !location.pathname.startsWith('/admin');
-
-  return (
-    <>
-    <NavBar></NavBar>
-    <CartProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<LoginView/>}/>
-        <Route path="/menu" element={<Menu/>}/>
-        <Route path="/register" element={<RegisterView/>}/>
-        <Route path="/checkout" element={<Checkout/>}/>
-        <Route path="/admin/*" element={<AdminOptions/>}/>
-      </Routes> 
-    </BrowserRouter>
-    </CartProvider>
-      {shouldShowNavBar && <NavBar />} {/* Renderiza NavBar solo si no está en /admin */}
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/admin/*" element={<AdminOptions />} />
-          <Route path="/admin/products/addproduct" element={<AddProduct />} />
-        </Routes>
-      </div>
-    </>
   );
 }
 
