8a08c237cc6d8119be353423c37405fe54c86edb
MarceDS07 <marce.de.souza.25@gmail.com>
2025-02-05 11:44:13 -0300

    Nuevo SideScroll
    Terminado front de categoriesDisplay,
    productDisplay y empezado CategoryRow

---
+++ chefhub/vite-project/src/components/Home/CategoryRow.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './CategoryRow.css'
+import ProductDisplay from './ProductDisplay';
+
+export default function CategoryRow() { 
+    return (
+        <>
+        <section className='sideScroll'>
+                <h1>Sugerencias</h1>
+            <div className='bloques'>
+                <ProductDisplay />
+                <ProductDisplay />
+                <ProductDisplay />
+                <ProductDisplay />
+            </div>
+        </section>
+        </>
+    )
+}
