f7ee90c97285978e1d378d6f0267930b4e18fbd5
Marcos <dacostamarcos25@gmail.com>
2025-02-14 07:34:01 -0300

    update ip db

--- api/models/db.php
+++ api/models/db.php
@@ -3,7 +3,7 @@
 	* Database Connection
 	*/
 	class DbConnect {
-		private $server = '192.168.0.10:3306';
+		private $server = '192.168.0.2:3306';
 		private $dbname = 'chefhub_db';
 		private $user = 'codefy';
 		private $pass = 'codefyits2024';
