d80bdca0b6d53cae6d619f072bc571201d56bd86
Marcos <dacostamarcos25@gmail.com>
2025-02-15 17:00:55 -0300

    fix db.php

--- api/models/db.php
+++ api/models/db.php
@@ -1,5 +1,5 @@
 <?php
-require dirname(__DIR__, 2) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 	
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
