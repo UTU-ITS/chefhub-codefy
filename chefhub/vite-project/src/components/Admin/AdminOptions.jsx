849632119ebeee851bc2a902597f0152c0f47d4b
Marcos <dacostamarcos25@gmail.com>
2025-02-26 15:28:51 -0300

    fix

--- chefhub/vite-project/src/components/Admin/AdminOptions.jsx
+++ chefhub/vite-project/src/components/Admin/AdminOptions.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from 'react';
-import {UserContext} from '../../context/user';
-import { Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useContext, useEffect } from 'react';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context/user';
 import './AdminOptions.css';
 import AdminProducts from './Products/AdminProducts';
 import AdminEmpolyees from './Users/Empolyees/AdminEmpolyees';
@@ -13,11 +13,27 @@ import AdminReports from './Reports/AdminReports';
 import AdminCategory from './Category/AdminCategory';
 import AdminIngredient from './Ingredient/AdminIngredient';
 import AdminPreferences from './Preferences/AdminPreferences';
- 
+import NotFound from '../../App';
 
 export default function Options() {
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
     const [selectedOption, setSelectedOption] = useState('');
-    const {user} = useContext(UserContext);
+
+    useEffect(() => {
+        if (!user || !user.data) {
+            navigate('/');
+        }
+    }, [user, navigate]);
+
+    if (!user || !user.data) {
+        return null;
+    }
+
+    if (!user.data.cargo) {
+        return <Routes><Route path="*" element={<NotFound />} /></Routes>;
+    }
+
     const handleOptionClick = (option) => {
         setSelectedOption(option);
     };
@@ -26,134 +42,92 @@ export default function Options() {
 
     return (
         <div className="options-div">
-                <div className="sidebar">
-                    <div className="options-admin">
-                        <h3 className="options-title">ADMINISTRACIÓN</h3>
-                        <ul>
-                            <li>
-                                <Link
-                                    to="/admin/dashboard"
-                                    onClick={() => handleOptionClick('Dashboard')}
-                                >
-                                    Panel de Información
-                                </Link>
-                            </li>
-                            {user && user.data && (user.data.cargo === "Administrativo") && (
-                                <>
-                            <li>
-
-                                <Link
-                                    to="/admin/products"
-                                    onClick={() => handleOptionClick('Productos')}
-                                >
-                                    Productos
-                                </Link>
-                            </li>
-
-                            <li>
-                                <Link
-                                    to="/admin/category"
-                                    onClick={() => handleOptionClick('Categorias')}
-                                >
-                                    Categorias
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/admin/ingredient"
-                                    onClick={() => handleOptionClick('Ingrediente')}
-                                >
-                                    Ingrediente
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/admin/tables"
-                                    onClick={() => handleOptionClick('Tables')}
-                                >
-                                    Mesas
-                                </Link>
-                            </li>
-                            
-                            <li>
-                                <Link
-                                    to="/admin/employees"
-                                    onClick={() => handleOptionClick('Funcionarios')}
-                                >
-                                    Empleados
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/admin/customers"
-                                    onClick={() => handleOptionClick('Customers')}
-                                >
-                                    Clientes
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/admin/reports"
-                                    onClick={() => handleOptionClick('Reports')}
-                                >
-                                    Informes
-                                </Link>
-                            </li>
-                            </>)}
-                            {user && user.data && (user.data.cargo === "Chef" || user.data.cargo === "Mesero" || user.data.cargo === "Administrativo") && (
-                                <>   
-                            <li>
-                                <Link
-                                    to="/admin/orders"
-                                    onClick={() => handleOptionClick('Orders')}
-                                >
-                                    Pedidos
-                                </Link>
-                            </li>
-                            </> )}  
-                            {user && user.data && ( user.data.cargo === "Mesero" || user.data.cargo === "Administrativo") && (
-                                <>  
-                            <li>
-                                <Link
-                                    to="/admin/reservations"
-                                    onClick={() => handleOptionClick('reservations')}
-                                >
-                                    Reservas
-                                </Link>
-                            </li>
-                            </> )} 
-                            {user && user.data && (user.data.cargo === "Administrativo") && (
-                                <>
-                            <div className="line-separator"></div>
-                            <h3 className="options-title">CONFIGURACIÓN DEL PORTAL</h3>
-
-                            <li>
-                                <Link
-                                    to="/admin/schedules"
-                                    onClick={() => handleOptionClick('Horarios')}
-                                >
-                                    Personalizacion y preferencias
-                                </Link>
-                            </li>
-                            </> )} 
-                        </ul>
-                    </div>
+            <div className="sidebar">
+                <div className="options-admin">
+                    <h3 className="options-title">ADMINISTRACIÓN</h3>
+                    <ul>
+                        <li>
+                            <Link to="/admin/dashboard" onClick={() => handleOptionClick('Dashboard')} className={isSelected('Dashboard')}>
+                                Panel de Información
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/products" onClick={() => handleOptionClick('Productos')} className={isSelected('Productos')}>
+                                Productos
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/category" onClick={() => handleOptionClick('Categorias')} className={isSelected('Categorias')}>
+                                Categorias
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/ingredient" onClick={() => handleOptionClick('Ingrediente')} className={isSelected('Ingrediente')}>
+                                Ingrediente
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/orders" onClick={() => handleOptionClick('Orders')} className={isSelected('Orders')}>
+                                Pedidos
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/reservations" onClick={() => handleOptionClick('reservations')} className={isSelected('reservations')}>
+                                Reservas
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/tables" onClick={() => handleOptionClick('Tables')} className={isSelected('Tables')}>
+                                Mesas
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/employees" onClick={() => handleOptionClick('Funcionarios')} className={isSelected('Funcionarios')}>
+                                Empleados
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/customers" onClick={() => handleOptionClick('Customers')} className={isSelected('Customers')}>
+                                Clientes
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/reports" onClick={() => handleOptionClick('Reports')} className={isSelected('Reports')}>
+                                Informes
+                            </Link>
+                        </li>
+                        <div className="line-separator"></div>
+                        <h3 className="options-title">CONFIGURACIÓN DEL PORTAL</h3>
+                        <li>
+                            <Link to="/admin/personalizacion" onClick={() => handleOptionClick('Personalización del portal')} className={isSelected('Personalización del portal')}>
+                                Personalización
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/preferences" onClick={() => handleOptionClick('Preferencias')} className={isSelected('Preferencias')}>
+                                Preferencias
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
-
+            </div>
             <div className="content">
-                    <Routes>
-                        <Route path="dashboard" element={<Dashboard />} />
-                        <Route path="products" element={<AdminProducts />} />
-                        <Route path="employees" element={<AdminEmpolyees />} />
-                        <Route path="customers" element={<AdminCustomers />} />
-                        <Route path="tables" element={<AdminTables />} />
-                        <Route path="reservations" element={<AdminReservations />} />
-                        <Route path="orders" element={<AdminOrders/>} />
-                        <Route path="reports" element={<AdminReports />} />
-                        <Route path="category" element={<AdminCategory />} />
-                        <Route path="ingredient" element={<AdminIngredient />} />
-                        <Route path="schedules" element={<AdminPreferences />} />
-                    </Routes>
+                <Routes>
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="personalizacion" element={<h1>Personalización del portal</h1>} />
+                    <Route path="products" element={<AdminProducts />} />
+                    <Route path="employees" element={<AdminEmpolyees />} />
+                    <Route path="customers" element={<AdminCustomers />} />
+                    <Route path="preferencias" element={<h1>Preferencias</h1>} />
+                    <Route path="tables" element={<AdminTables />} />
+                    <Route path="reservations" element={<AdminReservations />} />
+                    <Route path="orders" element={<AdminOrders />} />
+                    <Route path="reports" element={<AdminReports />} />
+                    <Route path="category" element={<AdminCategory />} />
+                    <Route path="ingredient" element={<AdminIngredient />} />
+                    <Route path="preferences" element={<AdminPreferences />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
             </div>
         </div>
     );
